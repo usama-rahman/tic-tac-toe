7559d48512e99ada34c9910b96954447c2ca4146
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-12 16:00:48 +0600

    Working

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,5 +1,55 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+const Square = ({ onSquareClick, value }) => {
+  return (
+    <button
+      onClick={onSquareClick}
+      className="border m-1 text-lg h-12 w-12 gap-2 border-gray-900"
+    >
+      {value}
+    </button>
+  );
+};
+
 const Game = () => {
-  return <div>game</div>;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+
+    if (isNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setIsNext(!isNext);
+  };
+
+  return (
+    <>
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 };
 
 export default Game;
