8fa6450ed0f021c1e3a186b802b60e02d633cd46
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-21 21:37:29 +0600

    Restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,52 @@
 const App = () => {
-  return <div>App</div>;
+  const handleSquareClick = () => {};
+  return (
+    <>
+      <div className="flex items-center justify-center w-full h-screen bg-black text-white">
+        <div className=" flex flex-col">
+          <Square suareClick={() => handleSquareClick(0)} />
+          <Square />
+          <Square />
+        </div>
+        <div className=" flex flex-col ">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className=" flex flex-col ">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
+
+const Square = () => {
+  return (
+    <button className="w-16 h-16 border-[2px] border-white text-3xl m-2 font-semibold rounded-lg "></button>
+  );
+};
+
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
---
+++ src/components/Square.jsx
@@ -0,0 +1,9 @@
+const Square = () => {
+  return (
+    <button className="w-16 h-16 border-[2px] border-white text-3xl m-2 font-semibold rounded-lg ">
+      X
+    </button>
+  );
+};
+
+export default Square;
